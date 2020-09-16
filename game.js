4d713ba005d70398b92c04e1ad94284bdfefe04d
Leonardo Ascenzi <lascenzi@colgate.edu>
2020-09-15 22:51:07 -0400

    finished boilerplate, hoping for no bugs

---
+++ game.js
@@ -0,0 +1,82 @@
+//COLORS
+var Colors = {
+    skyBlue: 0x86ebcc,
+    ground: 0x332609,
+    golden: 0xebaf2a,
+    white: 0xffffff,
+    grey: 0xc7c9c8
+};
+
+// THREEJS RELATED VARIABLES
+
+var camera, fieldOfView, aspectRatio, nearPlane, farPlane,
+    renderer, container, scene;
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var controls;
+
+//SCREEN & MOUSE VARIABLES
+
+var HEIGHT, WIDTH
+
+//INIT THREE JS, SCREEN AND MOUSE EVENTS
+
+function createCameraRender() {
+
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+
+  aspectRatio = WIDTH / HEIGHT;
+  fieldOfView = 60;
+  nearPlane = 1;
+  farPlane = 10000;
+  camera = new THREE.PerspectiveCamera(
+      fieldOfView,
+      aspectRatio,
+      nearPlane,
+      farPlane
+    );
+  //
+  camera.position.x = 0;
+  camera.position.z = 120;
+  camera.position.y = 40;
+
+
+  renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  renderer.setSize(WIDTH, HEIGHT);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+  container = document.getElementById('glcanvas');
+  container.appendChild(renderer.domElement);
+
+
+  window.addEventListener('resize', handleWindowResize, false);
+  window.addEventListener('mousemove', onMouseMove, false);
+  window.addEventListener('mousedown', onMouseDown, false);
+}
+
+// HANDLE SCREEN EVENTS
+function handleWindowResize() {
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+
+  renderer.setSize(WIDTH, HEIGHT);
+  camera.aspect = WIDTH / HEIGHT;
+  camera.updateProjectionMatrix();
+}
+
+//THIS IS THE GAME LOOP
+function loop() {
+  renderer.render(scene, camera);
+
+}
+
+function initGame() {
+
+  THREEx.FullScreen.bindKey({ charCode : 'l'.charCodeAt(0) });
+  document.onkeydown = handleKeyDown;
+  document.onkeyup = handleKeyUp;
+
+}
---
+++ input.js
@@ -0,0 +1,19 @@
+function handleKeyUp(keyEvent){
+  if(keyEvent.key == "l"){
+    alert("FUNCTIONING");
+
+  }
+}
+
+function handleKeyDown(keyEvent){
+
+}
+
+function onMouseMove(event){
+      mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
+function onMouseDown(event){
+
+}
